004f8402ea667966c045c3ffded344912a7b74e8
Felipe Balbi <felipe@balbi.sh>
2023-04-30 20:06:10 +0300

    learn-bevy: episode 1
    https://www.youtube.com/watch?v=TQt-v_bFdao&list=PLVnntJRoP85JHGX7rGDu6LaF3fmDDbqyd
    
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

---
+++ src/main.rs
@@ -0,0 +1,84 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(PeoplePlugin)
+        .run()
+}
+
+pub struct PeoplePlugin;
+
+impl Plugin for PeoplePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(setup)
+            .add_system(print_names)
+            .add_system(people_with_jobs)
+            .add_system(people_ready_for_hire)
+            .add_system(person_does_job);
+    }
+}
+
+pub fn setup(mut commands: Commands) {
+    commands.spawn((
+        Person {
+            name: "John".to_string(),
+        },
+        Employed {
+            job: Job::Programmer,
+        },
+    ));
+    commands.spawn((
+        Person {
+            name: "Jack".to_string(),
+        },
+        Employed { job: Job::Manager },
+    ));
+    commands.spawn(Person {
+        name: "Joseph".to_string(),
+    });
+    commands.spawn(Person {
+        name: "Jerry".to_string(),
+    });
+}
+
+pub fn print_names(person_query: Query<&Person>) {
+    for person in person_query.iter() {
+        println!("Name: {}", person.name);
+    }
+}
+
+pub fn people_with_jobs(person_query: Query<&Person, With<Employed>>) {
+    for person in person_query.iter() {
+        println!("{} has a job", person.name);
+    }
+}
+
+pub fn people_ready_for_hire(person_query: Query<&Person, Without<Employed>>) {
+    for person in person_query.iter() {
+        println!("{} is ready for a job", person.name);
+    }
+}
+
+pub fn person_does_job(person_query: Query<(&Person, &Employed)>) {
+    for (person, employed) in person_query.iter() {
+        println!("{} is a {:?}", person.name, employed.job);
+    }
+}
+
+#[derive(Component)]
+pub struct Person {
+    pub name: String,
+}
+
+#[derive(Component)]
+pub struct Employed {
+    pub job: Job,
+}
+
+#[derive(Debug)]
+pub enum Job {
+    Manager,
+    Programmer,
+    Tester,
+}
